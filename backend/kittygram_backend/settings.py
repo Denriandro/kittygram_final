9f33cfed758c0f4d72043d3e3979a5ef499d776f
Evgeniy Galeev <pioneer080@mail.ru>
2023-09-19 08:25:34 +0500

    Add allowed hosts

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,13 +7,13 @@ from pathlib import Path
 # load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 # SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['backend', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
